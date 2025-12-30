71c308669f14aeb416b4157f22d7fb750a3af079
Henrique Marangoni <henrique.1108@hotmail.com>
2025-12-30 20:24:40 -0300

    Feats: Corrigindo CORS para aceitar o Netlify

--- backend/src/main/java/br/com/henrique/bloodcrown_cs/Security/WebSecurityConfig.java
+++ backend/src/main/java/br/com/henrique/bloodcrown_cs/Security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package br.com.henrique.bloodcrown_cs.Security;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -61,22 +62,22 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-
+        
         configuration.setAllowedOrigins(Arrays.asList(
-            "http://127.0.0.1:5500",
-                "http://localhost:5500"
+            "http://localhost:5500",      
+            "http://localhost:8080",      
+            "http://127.0.0.1:5500",      
+            "https://bloodcrown.netlify.app" 
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true); 
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
         source.registerCorsConfiguration("/**", configuration);
-
         return source;
     }
 }
